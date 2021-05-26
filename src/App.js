753bea5500737e9611bcc34a27dee2635dc93ad2
Marcel <marcel-klasse@users.noreply.github.com>
2021-05-26 16:56:06 +0200

    so we begin

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [title, setTitle] = useState('');
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form>
+        <label htmlfor="title">Title of your Project</label>
+        <input type="text" id="title" value={title} onChange={(e) => setTitle(e.target.value)} />
+        </form>
     </div>
   );
 }
