838f002b4de7dcfc980e8931e77a494db1babb31
Marcel <marcel-klasse@users.noreply.github.com>
2021-06-16 11:55:29 +0200

    add profile

--- src/routes/profile/index.js
+++ src/routes/profile/index.js
@@ -1,14 +1,36 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useAuth } from '../../Auth'
+import useJoinedSpaces from '../../components/matrix_joined_spaces'
+import { Loading } from '../../components/loading'
 
 const Profile = () => {
   const auth = useAuth()
   const profile = auth.user
+  const {joinedSpaces, spacesErr, fetchSpaces} = useJoinedSpaces()
+
+
+  const Drafts = () => {
+    const [drafts, setDrafts] = useState([]);
+
+    return (
+      <>
+      <h2>Drafts:</h2>
+      <ul>
+          {spacesErr ? console.error(spacesErr) : joinedSpaces ? <p>You have <strong>{joinedSpaces.length}</strong> projects.</p>
+            /*  <li key={index} ><button onClick={() => { setProjectSpace(space.room_id); setTitle(space.name); setVisibility(space.published) }}>{space.name}</button></li>)*/
+ : null 
+        }
+      </ul>
+      </>
+    )
+  }
 
   return (
     <div>
       <p>Hello <strong>{profile.displayname}</strong></p>
       <p>welcome to your profile for the Rundgang 2021.</p>
+
+      { fetchSpaces ? <Loading /> : <Drafts />}
     </div>
   )
 }
