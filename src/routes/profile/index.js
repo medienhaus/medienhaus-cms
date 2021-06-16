b4227d060b9d9d2c673ff4f4e194832703137cdb
Marcel <marcel-klasse@users.noreply.github.com>
2021-06-16 13:06:20 +0200

    profile now showing published and unpublished projects

--- src/routes/profile/index.js
+++ src/routes/profile/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React from 'react'
 import { useAuth } from '../../Auth'
 import useJoinedSpaces from '../../components/matrix_joined_spaces'
 import { Loading } from '../../components/loading'
@@ -9,29 +9,42 @@ const Profile = () => {
   const {joinedSpaces, spacesErr, fetchSpaces} = useJoinedSpaces()
 
 
-  const Drafts = () => {
-    const [drafts, setDrafts] = useState([]);
-
+  const Projects = () => {
+    var draftCounter = 0;
     return (
       <>
-      
+        {joinedSpaces && <p>You have <strong>{joinedSpaces.filter(x => x.published === "invite").length}</strong> drafts, which are not publicly visible.</p>}
+        
       <ul>
-          {spacesErr ? console.error(spacesErr) : joinedSpaces ? <p>You have <strong>{joinedSpaces.length}</strong> projects.</p>
-            /*  <li key={index} ><button onClick={() => { setProjectSpace(space.room_id); setTitle(space.name); setVisibility(space.published) }}>{space.name}</button></li>)*/
- : null 
+          {spacesErr ? console.error(spacesErr) : joinedSpaces?.filter(x => x.published === "invite").map((space, index) => {
+              return <li>{space.name}</li>
+      }) 
         }
         </ul>
-        <h2>Drafts:</h2>
+        
+        {joinedSpaces && <p>You have <strong>{joinedSpaces.filter(x => x.published === "public").length}</strong> published projects, which are publicly visible.</p>}
+        
+        <ul>
+            {spacesErr ? console.error(spacesErr) : joinedSpaces?.filter(x => x.published === "public").map((space, index) => {
+                return <li>{space.name}</li>
+        }) 
+          }
+        </ul>
       </>
     )
   }
+
+
 
   return (
     <div>
       <p>Hello <strong>{profile.displayname}</strong></p>
       <p>welcome to your profile for the Rundgang 2021.</p>
 
-      { fetchSpaces ? <Loading /> : <Drafts />}
+      {fetchSpaces ? <Loading /> : (
+        <Projects />
+      
+      )}
     </div>
   )
 }
