3fb2a42ba8ad235814eee905c64561fc85f280e5
Marcel <marcel-klasse@users.noreply.github.com>
2021-06-18 11:23:38 +0200

    fixed reload issues by not using client for reloading

--- src/routes/submit/AddContent/index.js
+++ src/routes/submit/AddContent/index.js
@@ -4,15 +4,13 @@ import AddBlock from './AddBlock';
 
 const AddContent = ({number, projectSpace, blocks, reloadProjects}) => {
   const [contentSelect, setContentSelect] = useState('');
-  const [isPlusButton, setIsPlusButton] = useState(false);
+  const [isPlusButton, setIsPlusButton] = useState(true);
   
   const displayPlusButton = (button) => {
     setIsPlusButton(button)
   }
-
-
     return (
-        !isPlusButton ? <button key={'add' + number} onClick={(e) => { e.preventDefault(); setIsPlusButton(true) }} >+</button> : (
+        isPlusButton ? <button key={'add' + number} onClick={(e) => { e.preventDefault(); setIsPlusButton(false) }} >+</button> : (
         <div>
               <select name="content-select"  defaultValue={''} id="content-select" onChange={(e) => setContentSelect(e.target.value)}>
               <option value='' disabled={true} >Select Content</option>
@@ -28,7 +26,7 @@ const AddContent = ({number, projectSpace, blocks, reloadProjects}) => {
                 {/*
             // fetch("https://stream.udk-berlin.de/api/userId/myVideos")
             */}
-          
+          <button onClick={(e) => { e.preventDefault(); setIsPlusButton(true)}} >CANCEL</button>
             </div>)
     )
 }
