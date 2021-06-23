49861f56b724cb4a7b43dda0be93e4c1d851af06
Marcel <marcel-klasse@users.noreply.github.com>
2021-06-23 23:23:31 +0200

    trying to overlay highlight.js with textarea

--- src/routes/submit/DisplayContent/Code/index.js
+++ src/routes/submit/DisplayContent/Code/index.js
@@ -18,6 +18,34 @@ const Code = ({ onSave, storage, saved, content }) => {
       <p>{saved}</p>
       {<div dangerouslySetInnerHTML={{ __html: hljs.highlightAuto(value).value }}></div>}
     </pre>
+
+    /* Trying to overlay the two divy 
+<div style={{
+  position: "relative",
+}}>
+  <pre>
+    <code className="json" style={{
+      position: "absolute",
+      left: "0", top: "0", right: "0", bottom: "0",
+      width: "100%",
+      height: "100%",
+    }}>
+      <textarea
+        style={{ height: '100%', opacity: "0.1" }}
+        onChange={e => setValue(e.target.value)}
+        onBlur={(e) => { storage(value); onSave(e) }}
+        value={value}
+      />
+    </code>
+
+    {<div style={{
+      zIndex: "4",
+      opacity: "1"
+    }} dangerouslySetInnerHTML={{ __html: hljs.highlightAuto(value).value }}></div>}
+  </pre>
+  <p>{saved}</p>
+</div>
+*/
   )
 }
 export default Code
