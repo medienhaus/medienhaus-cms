0a67fb32a3e6ee2b6075464b75ef6da77abf8ed7
Marcel <marcel-klasse@users.noreply.github.com>
2021-06-24 10:45:31 +0200

    add request access to context spaces functionality in Category

--- src/routes/submit/Category/index.js
+++ src/routes/submit/Category/index.js
@@ -1,31 +1,38 @@
 import React, { useState } from 'react'
 import Matrix from '../../../Matrix'
+import Request from './Request'
 import { Loading } from '../../../components/loading'
 
 const Category = () => {
   const [subject, setSubject] = useState('')
   const [loading, setLoading] = useState(false)
+  const [member, setMember] = useState(false);
   const matrixClient = Matrix.getMatrixClient()
 
   const isMember = async (e) => {
     e.preventDefault()
     setLoading(true)
     setSubject(e.target.value)
-    const member = await matrixClient.members(e.target.value + localStorage.getItem('mx_home_server')).catch(err => console.error(err)).then(console.log)
-    console.log(member?.chunk)
+    await matrixClient.members(e.target.value + localStorage.getItem('mx_home_server')).catch(err => console.error(err)).then(res => {
+      setMember(res.chunk.map(a => a.sender).includes(localStorage.getItem('mx_user_id')))
+    })
+    console.log(member);
     setLoading(false)
   }
+
+
   return (
     <div>
       <label htmlFor="subject">Studiengang</label>
       <select id="subject" name="subject" defaultValue={''} value={subject} onChange={(e) => isMember(e)}>
         <option value="" disabled={true} >Select Content</option>
         <option value="!KIIwpwkcYdtsTgSGhw:">New Media</option>
-        <option value="act">Schauspiel</option>
+        <option value="!qKuBiJVgPrRowlcwTG:">Digitale Klasse</option>
         <option value="clown">Clown</option>
         <option value="kunst">Kunst</option>
       </select>
       {loading && <Loading />}
+      {!member && subject && <Request roomId={subject} />}
       {
         // sollte es hier die möglichkeit geben mehrere auszuwählen? also studiengang übergreifende projekte
       }
