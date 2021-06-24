edc81073d3c4c747b6e7802457eb3801b7bf2dac
Marcel <marcel-klasse@users.noreply.github.com>
2021-06-24 14:00:30 +0200

    parsing knock room and door room in value

--- src/routes/submit/Category/index.js
+++ src/routes/submit/Category/index.js
@@ -5,6 +5,7 @@ import { Loading } from '../../../components/loading'
 
 const Category = ({ title, projectSpace }) => {
   const [subject, setSubject] = useState('')
+  const [knock, setKnock] = useState('');
   const [loading, setLoading] = useState(false)
   const [member, setMember] = useState(false);
   const matrixClient = Matrix.getMatrixClient()
@@ -12,11 +13,19 @@ const Category = ({ title, projectSpace }) => {
   const isMember = async (e) => {
     e.preventDefault()
     setLoading(true)
+    const rooms = JSON.parse(e.target.value);
     setSubject(e.target.value)
-    await matrixClient.members(e.target.value + localStorage.getItem('mx_home_server')).catch(err => console.error(err)).then(res => {
-      setMember(res.chunk.map(a => a.sender).includes(localStorage.getItem('mx_user_id')))
-    })
-    console.log(member);
+    setKnock(rooms.knock)
+    try {
+      await matrixClient.members(rooms.space + localStorage.getItem('mx_home_server')).catch(err => console.error(err)).then(res => {
+        setMember(res.chunk.map(a => a.sender).includes(localStorage.getItem('mx_user_id')))
+      })
+      console.log(member);
+    } catch (err) {
+      console.error(err)
+      setMember(false)
+    }
+
     setLoading(false)
   }
   const callback = (requested) => {
@@ -28,12 +37,12 @@ const Category = ({ title, projectSpace }) => {
     <div>
       <label htmlFor="subject">Studiengang</label>
       <select id="subject" name="subject" defaultValue={''} value={subject} onChange={(e) => isMember(e)}>
-        <option value="" disabled={true} >Select Content</option>
-        <option value="!JaLRUAZnONCuUHMPvy:">New Media</option>
-        <option value="!rorMnDkmfIThdFzwPD:">Digitale Klasse</option>
+        <option value="" disabled={true} >Select Context</option>
+        <option value={JSON.stringify({ knock: "!JaLRUAZnONCuUHMPvy:", space: "!KIIwpwkcYdtsTgSGhw:" })}>New Media</option>
+        <option value={JSON.stringify({ knock: "!rorMnDkmfIThdFzwPD:", space: "qKuBiJVgPrRowlcwTG" })}>Digitale Klasse</option>
       </select>
       {loading && <Loading />}
-      {!member && subject && <Knock roomId={subject} projectSpace={projectSpace} title={title} callback={callback} />}
+      {!member && subject && <Knock roomId={knock} projectSpace={projectSpace} title={title} callback={callback} />}
       {
         // sollte es hier die möglichkeit geben mehrere auszuwählen? also studiengang übergreifende projekte
       }
