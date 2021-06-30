171f31503bae2e292caf6f43ffb34cf079d531c0
Marcel <marcel-klasse@users.noreply.github.com>
2021-06-30 18:22:51 +0200

    change power level for content rooms

--- src/routes/submit/matrix_create_room/index.js
+++ src/routes/submit/matrix_create_room/index.js
@@ -24,7 +24,24 @@ const createBlock = async (e, content, number, space) => {
     }, {
       type: 'm.room.history_visibility',
       content: { history_visibility: 'world_readable' }
-    }]
+    }],
+    power_level_content_override: {
+      "ban": 50,
+      "events": {
+        "m.room.name": 50,
+        "m.room.power_levels": 50
+      },
+      "events_default": 0,
+      "invite": 50,
+      "kick": 50,
+      "notifications": {
+        "room": 20
+      },
+      "redact": 50,
+      "state_default": 50,
+      "users_default": 0
+
+    }
   }
 
   const req = {
