721cb0fedc2848f026f001bf06563605f5b184af
Marcel <marcel-github@klasse.io>
2021-07-07 12:04:22 +0200

    add conditionals to licenses

--- src/components/FileUpload/index.js
+++ src/components/FileUpload/index.js
@@ -36,34 +36,34 @@ const FileUpload = (props) => {
               <option value="rs-inc">In Copyright</option>
             </optgroup>
           </select>
-          <div id="cc0" className="license-info">
+          {license === 'cc0' && <div id="cc0" className="license-info">
             <p>CC0 (aka CC Zero) is a public dedication tool, which allows creators to give up their copyright and put their works into the worldwide public domain. CC0 allows reusers to distribute, remix, adapt, and build upon the material in any medium or format, with no conditions.</p>
             <p>CC BY includes the following elements:</p>
-          </div>
-          <div id="cc-by" className="license-info">
+          </div>}
+          {license === 'cc-by' && <div id="cc-by" className="license-info">
             <p>This license allows reusers to distribute, remix, adapt, and build upon the material in any medium or format, so long as attribution is given to the creator. The license allows for commercial use.</p>
             <p>CC BY includes the following elements:</p>
             <ul>
               <li>BY: Credit must be given to the creator</li>
             </ul>
-          </div>
-          <div id="cc-by-sa" className="license-info">
+          </div>}
+          {license === 'cc-by-sa' && <div id="cc-by-sa" className="license-info">
             <p>This license allows reusers to distribute, remix, adapt, and build upon the material in any medium or format, so long as attribution is given to the creator. The license allows for commercial use. If you remix, adapt, or build upon the material, you must license the modified material under identical terms.</p>
             <p>CC BY-SA includes the following elements:</p>
             <ul>
               <li>BY: Credit must be given to the creator</li>
               <li>SA: Adaptations must be shared under the same terms</li>
             </ul>
-          </div>
-          <div id="cc-by-nc" className="license-info">
+          </div>}
+          {license === 'cc-by-nc' && <div id="cc-by-nc" className="license-info">
             <p>This license allows reusers to distribute, remix, adapt, and build upon the material in any medium or format for noncommercial purposes only, and only so long as attribution is given to the creator.</p>
             <p>CC BY-NC includes the following elements:</p>
             <ul>
               <li>BY: Credit must be given to the creator</li>
               <li>NC: Only noncommercial uses of the work are permitted</li>
             </ul>
-          </div>
-          <div id="cc-by-nc-sa" className="license-info">
+          </div>}
+          {license === 'cc-by-nc-sa' && <div id="cc-by-nc-sa" className="license-info">
             <p>This license allows reusers to distribute, remix, adapt, and build upon the material in any medium or format for noncommercial purposes only, and only so long as attribution is given to the creator. If you remix, adapt, or build upon the material, you must license the modified material under identical terms.</p>
             <p>CC BY-NC-SA includes the following elements:</p>
             <ul>
@@ -71,16 +71,16 @@ const FileUpload = (props) => {
               <li>NC: Only noncommercial uses of the work are permitted</li>
               <li>SA: Adaptations must be shared under the same terms</li>
             </ul>
-          </div>
-          <div id="cc-by-nd" className="license-info">
+          </div>}
+          {license === 'cc-by-nd' && <div id="cc-by-nd" className="license-info">
             <p>This license allows reusers to copy and distribute the material in any medium or format in unadapted form only, and only so long as attribution is given to the creator. The license allows for commercial use.</p>
             <p>CC BY-ND includes the following elements:</p>
             <ul>
               <li>BY: Credit must be given to the creator</li>
               <li>ND: No derivatives or adaptations of the work are permitted</li>
             </ul>
-          </div>
-          <div id="cc-by-nc-nd" className="license-info">
+          </div>}
+          {license === 'cc-by-nc-nd' && <div id="cc-by-nc-nd" className="license-info">
             <p>This license allows reusers to copy and distribute the material in any medium or format in unadapted form only, for noncommercial purposes only, and only so long as attribution is given to the creator.</p>
             <p>CC BY-NC-ND includes the following elements:</p>
             <ul>
@@ -88,7 +88,7 @@ const FileUpload = (props) => {
               <li>NC: Only noncommercial uses of the work are permitted</li>
               <li>ND: No derivatives or adaptations of the work are permitted</li>
             </ul>
-          </div>
+          </div>}
           <p>Further information:</p>
           <ul>
             <li><a href="https://creativecommons.org/about/cclicenses/" rel="nofollow noreferrer external" target="_blank">About CC Licenses</a></li>
