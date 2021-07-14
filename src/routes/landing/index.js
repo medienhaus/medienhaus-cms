201942a3bfc130e4e7c9511bad8ba0c8dbee7562
Florian Weber <florian@weber.world>
2021-07-14 14:09:18 +0200

    Add locales for landing page

--- src/routes/landing/index.js
+++ src/routes/landing/index.js
@@ -1,37 +1,38 @@
 import React from 'react'
 import { Trans, useTranslation } from 'react-i18next'
+import { useAuth } from '../../Auth'
+import { NavLink } from 'react-router-dom'
 
 const Landing = () => {
   const { t } = useTranslation('landing')
+  const auth = useAuth()
+
   return (
     <div>
-      <Trans t={t} i18nKey="welcome">
-        <strong>Welcome to the Rundgang 2021 content management system!</strong>
-        <p>The Rundgang – Open Days of the Berlin University of the Arts will take place for the first time in both analogue and digital space from 29–31 October 2021. On this platform, you will have the opportunity to create your own projects and thus add information on analogue offers or present digitally created semester projects.</p>
-        <p>We also offer you the possibility to offer virtual meetings via the video conferencing tool BigBlueButton at <a href="https://meetings.udk-berlin.de" rel="external nofollow noopener noreferrer" target="_blank">udk/meet</a> and to integrate videos and live streams via <a href="https://stream.udk-berlin.de" rel="external nofollow noopener noreferrer" target="_blank">udk/stream</a> on your own project page.</p>
-        <p>As the platform is a new tool that can continue to enrich the Rundgang – Open Days in the future, we would be happy if you send us your feedback on how to handle the system via the <a href="/feedback">/feedback</a> link in the menu.</p>
-        <p>If you need technical support for entering projects, please fill out the form via the <a href="/support">/support</a> link, which can also be found in the menu after logging in.</p>
-        <p><em>Please note: The public-facing Rundgang website on which all projects will be presented, is still under construction. Therefore, it is not yet possible to preview the projects that have been created.</em></p>
-      </Trans>
+      <strong>{t('Welcome to the Rundgang 2021 content management system!')}</strong>
+      <p>{t('The Rundgang – Open Days of the Berlin University of the Arts will take place for the first time in both analogue and digital space from 29–31 October 2021. On this platform, you will have the opportunity to create your own projects and thus add information on analogue offers or present digitally created semester projects.')}</p>
+      {auth.user && (
+        <>
+          <p>
+            <Trans t={t} i18nKey="bbbAndPeertube">
+              We also offer you the possibility to offer virtual meetings via the video conferencing tool BigBlueButton at <a href="https://meetings.udk-berlin.de" rel="external nofollow noopener noreferrer" target="_blank">udk/meet</a> and to integrate videos and live streams via <a href="https://stream.udk-berlin.de" rel="external nofollow noopener noreferrer" target="_blank">udk/stream</a> on your own project page.
+            </Trans>
+          </p>
+          <p>
+            <Trans t={t} i18nKey="feedback">
+              As the platform is a new tool that can continue to enrich the Rundgang – Open Days in the future, we would be happy if you send us your feedback on how to handle the system via the <NavLink to="/feedback">/feedback</NavLink> link in the menu.
+            </Trans>
+          </p>
+          <p>
+            <Trans t={t} i18nKey="support">
+              If you need technical support for entering projects, please fill out the <NavLink to="/support">/support</NavLink> form.
+            </Trans>
+          </p>
+        </>
+      )}
+      <p><em>{t('Please note: The public-facing Rundgang website on which all projects will be presented, is still under construction. Therefore, it is not yet possible to preview the projects that have been created.')}</em></p>
     </div>
   )
 }
-/*
-<section id="team">
-        <h2><strong>Team</strong> (sorted alphabetically):</h2>
-        <ul>
-          <li>Alexej Bormatenkow</li>
-          <li>Dirk Erdmann</li>
-          <li><a href="mailto:kg@udk-berlin.de?subject=medienhaus/" rel="external nofollow noreferrer">Klaus Gasteier</a></li>
-          <li>Marcel Haupt</li>
-          <li><a href="mailto:dh@udk-berlin.de?subject=medienhaus/" rel="external nofollow noreferrer">Daniel Hromada</a></li>
-          <li>Frederik Müller</li>
-          <li>Andi Rueckel</li>
-          <li>Merani Schilcher</li>
-          <li><a href="mailto:rfws@udk-berlin.de?subject=medienhaus/" rel="external nofollow noreferrer">Robert Schnüll</a></li>
-          <li>Paul Seidler</li>
-        </ul>
-      </section>
-*/
 
 export default Landing
