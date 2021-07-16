d09a7528b750cbeef6387559f092546576e023a9
Marcel <marcel-klasse@users.noreply.github.com>
2021-07-16 18:11:50 +0200

    beginning to refactor

--- src/components/matrix_joined_spaces/index.js
+++ src/components/matrix_joined_spaces/index.js
@@ -1,62 +1,70 @@
 import { useEffect, useState } from 'react'
 import Matrix from '../../Matrix'
 
-/*
-Keeping this here as a safety for now
-const fetchIntroduction = async (room) => {
-  const introductionRoom = process.env.REACT_APP_MATRIX_BASE_URL + `/_matrix/client/r0/rooms/${room}/messages?limit=1&dir=b`
-  const introduction = await fetch(introductionRoom, {
-    method: 'GET',
-    headers: { Authorization: 'Bearer ' + localStorage.getItem('medienhaus_access_token') }
-  })
-  const introductionText = await introduction.json()
-  return introductionText.chunk[0].content.body
-}
-*/
-
 const getAnswer = async () => {
   const matrixClient = Matrix.getMatrixClient()
-  try {
-    // first we have to get all rooms a user has currently joined
-    const answer = await matrixClient.getJoinedRooms()
-    if (answer.joined_rooms.length > 0) {
-      const getNames = await Promise.all(answer.joined_rooms.map(async (roomId) => {
-        try {
-          // now we need to find out which of these rooms are spaces
-          const room = await matrixClient.getSpaceSummary(roomId)
-          if (room.rooms[0].room_type !== 'm.space') return
-          // and which one of these spaces has our medienhaus state event
-          const meta = await matrixClient.getStateEvent(room.rooms[0].room_id, 'dev.medienhaus.meta')
-          if (meta.type !== 'studentproject') return
-          // then we check if the project is a collaboration
-          const collab = room.rooms[0].num_joined_members > 1 ? await matrixClient.getJoinedRoomMembers(room.rooms[0].room_id) : false
-          // and if the project is published or still a draft
-          const joinRule = await fetch(process.env.REACT_APP_MATRIX_BASE_URL + `/_matrix/client/r0/rooms/${room.rooms[0].room_id}/state/m.room.join_rules/`, {
-            method: 'GET',
-            headers: { Authorization: 'Bearer ' + localStorage.getItem('medienhaus_access_token') }
-          })
-          const published = await joinRule.json()
-          // fetch introduction text
-          // const introduction = room.rooms[1] ? await fetchIntroduction(room.rooms[1].room_id) : false
-          return {
-            name: room.rooms[0].name,
-            room_id: room.rooms[0].room_id,
-            published: published.join_rule,
-            collab: collab && collab.joined,
-            avatar_url: room.rooms[0].avatar_url !== undefined && room.rooms[0].avatar_url,
-            description: room.rooms[0].topic,
-            meta: meta
-          }
-        } catch (error) {}
+
+  // before fetching we check if our initial sync is completed
+  console.log(matrixClient.isInitialSyncComplete())
+  if (matrixClient.isInitialSyncComplete()) {
+    const allRooms = matrixClient.getRooms()
+    console.log(allRooms)
+    const filteredRooms = allRooms.forEach(async room => {
+      // we filter all joined rooms for spaces and within those spaces we already filter language spaces
+      if (room.getType() !== 'm.space' || room.name === 'de' || room.name === 'en') return
+      return {
+        name: room.name
       }
-      )
-      )
-      return getNames.filter(Boolean)
-    } else {
-      return false
+      // const filter = room.timeline.filter(event => event.event.type === 'dev.medienhaus.meta')
+      /*
+      room.getAvatarUrl()
+      room.getJoinRule()
+      room.getJoinedMembers()
+      room.members */
+    })
+    console.log(filteredRooms)
+    try {
+      // first we have to get all rooms a user has currently joined
+      const answer = await matrixClient.getJoinedRooms()
+      if (answer.joined_rooms.length > 0) {
+        const getNames = await Promise.all(answer.joined_rooms.map(async (roomId) => {
+          try {
+            // now we need to find out which of these rooms are spaces
+            const room = await matrixClient.getSpaceSummary(roomId)
+            if (room.rooms[0].room_type !== 'm.space') return
+            // and which one of these spaces has our medienhaus state event
+            const meta = await matrixClient.getStateEvent(room.rooms[0].room_id, 'dev.medienhaus.meta')
+            if (meta.type !== 'studentproject') return
+            // then we check if the project is a collaboration
+            const collab = room.rooms[0].num_joined_members > 1 ? await matrixClient.getJoinedRoomMembers(room.rooms[0].room_id) : false
+            // and if the project is published or still a draft
+            const joinRule = await fetch(process.env.REACT_APP_MATRIX_BASE_URL + `/_matrix/client/r0/rooms/${room.rooms[0].room_id}/state/m.room.join_rules/`, {
+              method: 'GET',
+              headers: { Authorization: 'Bearer ' + localStorage.getItem('medienhaus_access_token') }
+            })
+            const published = await joinRule.json()
+            // fetch introduction text
+            // const introduction = room.rooms[1] ? await fetchIntroduction(room.rooms[1].room_id) : false
+            return {
+              name: room.rooms[0].name,
+              room_id: room.rooms[0].room_id,
+              published: published.join_rule,
+              collab: collab && collab.joined,
+              avatar_url: room.rooms[0].avatar_url !== undefined && room.rooms[0].avatar_url,
+              description: room.rooms[0].topic,
+              meta: meta
+            }
+          } catch (error) { }
+        }
+        )
+        )
+        return getNames.filter(Boolean)
+      } else {
+        return false
+      }
+    } catch (e) {
+      console.log(e)
     }
-  } catch (e) {
-    console.log(e)
   }
 }
 
