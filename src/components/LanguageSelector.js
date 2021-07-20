153dca1cb8631a17e3b73c18cced78d3d5ea9a4b
Florian Weber <florian@weber.world>
2021-07-20 16:29:09 +0200

    Mobile viewports when logged out: Always show /login in header and language selector in footer

---
+++ src/components/LanguageSelector.js
@@ -0,0 +1,19 @@
+import i18n from 'i18next'
+import React from 'react'
+
+const LanguageSelector = () => {
+  const changeLanguage = event => {
+    const languageCode = event.target.value
+    localStorage.setItem('cr_lang', languageCode)
+    i18n.changeLanguage(languageCode)
+  }
+
+  return (
+    <select defaultValue={i18n.language} onChange={changeLanguage}>
+      <option value="en">EN</option>
+      <option value="de">DE</option>
+    </select>
+  )
+}
+
+export default LanguageSelector
