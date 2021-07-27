d154e51aa4dcce161266874fce497f5e6971e686
Marcel <marcel-klasse@users.noreply.github.com>
2021-07-27 10:59:58 +0200

    change alt text

--- src/routes/account/Avatar/avatar.js
+++ src/routes/account/Avatar/avatar.js
@@ -30,13 +30,15 @@ const Avatar = ({ avatarUrl, name }) => {
   }
 
   return (
+
     <div className="profile-image">
+      {console.log(name)}
       <h3>Profile image</h3>
       <p>Here you can change your profile image. The profile image will be shown on the public Rundgang website along with your project.</p>
-      {currentAvatar ? <img className="avatar" src={src} alt={'profile image of' + { name }} /> : <canvas className="avatar" />}
+      {currentAvatar ? <img className="avatar" src={src} alt={`profile avatar of ${name}`} /> : <canvas className="avatar" />}
       {!changeAvatar &&
         <button onClick={() => {
-          setChangeAvatar(changeAvatar => !changeAvatar)
+          setChangeAvatar(true)
         }}
         >{t('CHANGE')}
         </button>}
