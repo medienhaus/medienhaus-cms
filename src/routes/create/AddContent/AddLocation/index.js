3563fb9786e91280707d989ad70cbbcb7239c71b
Marcel <marcel-github@klasse.io>
2021-09-23 19:06:28 +0200

    add custom location leaflet

--- src/routes/create/AddContent/AddLocation/index.js
+++ src/routes/create/AddContent/AddLocation/index.js
@@ -1,6 +1,7 @@
-import React, { useState } from 'react'
+import React, { useMemo, useRef, useState } from 'react'
 import LoadingSpinnerButton from '../../../../components/LoadingSpinnerButton'
 import Matrix from '../../../../Matrix'
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
 import { useTranslation } from 'react-i18next'
 // assets
 import locations from '../../../../assets/data/locations.json'
@@ -8,33 +9,84 @@ import createBlock from '../../matrix_create_room'
 
 const AddLocation = ({ number, projectSpace, onBlockWasAddedSuccessfully, callback }) => {
   const [selectedLocation, setSelectedLocation] = useState('')
+  const [customLocation, setCustomLocation] = useState('')
   const [room, setRoom] = useState('')
   const [loading, setLoading] = useState(false)
   const matrixClient = Matrix.getMatrixClient()
   const { t } = useTranslation('locations')
-  console.log(selectedLocation)
+  const center = {
+    lat: 52.4908249640345,
+    lng: 13.359567285078
+  }
 
   const handleSubmit = async () => {
     setLoading(true)
+    const location = selectedLocation === 'custom' ? customLocation : selectedLocation
     await createBlock(undefined, 'location', number, projectSpace).then(async (res) =>
       await matrixClient.sendMessage(res, {
         msgtype: 'm.text',
-        body: selectedLocation + '-' + room
+        body: location + '-' + room
       })).catch(console.log)
     callback()
     onBlockWasAddedSuccessfully()
     setLoading(false)
   }
-  console.log(selectedLocation)
+
+  function DraggableMarker () {
+    const [position, setPosition] = useState(center)
+    const markerRef = useRef(null)
+
+    const eventHandlers = useMemo(
+      () => ({
+        dragend () {
+          const marker = markerRef.current
+          if (marker != null) {
+            setPosition(marker.getLatLng())
+            setCustomLocation(position.lat + ', ' + position.lng)
+          }
+        }
+      }),
+      []
+    )
+
+    return (
+      <Marker
+        draggable
+        eventHandlers={eventHandlers}
+        position={position}
+        ref={markerRef}
+      >
+        <Popup minWidth={90}>
+          <span>
+            lat: {position.lat}, lng: {position.lng}
+          </span>
+        </Popup>
+      </Marker>
+    )
+  }
+
   return (
     <>
       <select name="location-select" value={selectedLocation} id="location-select" onChange={(e) => setSelectedLocation(e.target.value)}>
         <option value="" disabled>{t('-- select venue --')}</option>
-        <option value="0.0, 0.0">{t('other venue, please enter below')}</option>
+        <option value="custom">{t('other venue, please enter below')}</option>
         {locations.map(location => <option value={location.coordinates} key={location.coordinates}>{location.name}</option>)}
       </select>
+
+      {selectedLocation === 'custom' && <>
+        <p>{t('Drag the marker to the desired location.')}</p>
+        <div className="map">
+          <MapContainer className="center" center={center} zoom={12} scrollWheelZoom={false}>
+            <TileLayer
+              attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
+            <DraggableMarker />
+          </MapContainer>
+        </div>
+      </>}
       <input type="text" placeholder={t('room number or specific location')} onChange={(e) => setRoom(e.target.value)} />
-      {selectedLocation === '0.0, 0.0' && <div>custom location</div>}
+
       <div className="confirmation">
         <button className="cancel" onClick={() => { callback() }}>{t('CANCEL')}</button>
         <LoadingSpinnerButton disabled={loading || !selectedLocation || (selectedLocation === '0.0, 0.0' && !room)} onClick={handleSubmit}>{t('SAVE')}</LoadingSpinnerButton>
