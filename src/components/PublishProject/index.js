1cfe259b4d0fbec7d0a1b3ce2138d9116c4ded88
Marcel <marcel-klasse@users.noreply.github.com>
2021-10-06 12:24:05 +0200

    check for context in right place

--- src/components/PublishProject/index.js
+++ src/components/PublishProject/index.js
@@ -9,7 +9,7 @@ const PublishProject = ({ disabled, space, published, time, metaEvent }) => {
   const [userFeedback, setUserFeedback] = useState()
   const [visibility, setVisibility] = useState(published)
   const matrixClient = Matrix.getMatrixClient()
-
+  console.log(metaEvent)
   useEffect(() => {
     if (published === 'invite') {
       // if 'published' = 'invite we know its legacy and we update it accordingly
@@ -81,11 +81,11 @@ const PublishProject = ({ disabled, space, published, time, metaEvent }) => {
     <div className="below">
       <select
         id="visibility" name="visibility" value={visibility} onChange={(e) => {
-          if (space.topic && space.context) onChangeVisibility(e.target.value)
+          if (space.topic && metaEvent.context) onChangeVisibility(e.target.value)
         }} disabled={disabled}
       >
         <option value="draft">{t('Draft')}</option>
-        <option value="public" disabled={!space.topic || !space.context}>{t('Public')}</option>
+        <option value="public" disabled={!space.topic || !metaEvent.context}>{t('Public')}</option>
       </select>
       {userFeedback && <p>{userFeedback}</p>}
     </div>
