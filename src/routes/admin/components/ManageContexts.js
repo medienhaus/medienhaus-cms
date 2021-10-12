a785dfdbd090bb77ffc25798d7f2f345b9158e10
Marcel <marcel-github@klasse.io>
2021-10-12 18:49:05 +0200

    remove removed spaces from tree

--- src/routes/admin/components/ManageContexts.js
+++ src/routes/admin/components/ManageContexts.js
@@ -1,16 +1,116 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useTranslation } from 'react-i18next'
 import CreateContext from './CreateContext'
 import { RemoveContext } from './RemoveContext'
+import * as _ from 'lodash'
+import { Loading } from '../../../components/loading'
 
 const ManageContexts = (props) => {
   const { t } = useTranslation('admin')
   const [selectedContext] = useState('!EyRTeozehwfsYePWMl:dev.medienhaus.udk-berlin.de')
+  const [structure, setStructure] = useState(null)
   const [newContext, setNewContext] = useState('')
   const [parentName] = useState('Stechlin')
   // eslint-disable-next-line no-unused-vars
   const [disableButton, setDisableButton] = useState(false)
   const [parent] = useState('!ZbMmIxgnJIhuROlgKJ:dev.medienhaus.udk-berlin.de')
+
+  const createStructurObject = async () => {
+    async function getSpaceStructure (matrixClient, motherSpaceRoomId, includeRooms, hierarchy) {
+      const result = {}
+
+      function createSpaceObject (id, name, metaEvent) {
+        return {
+          id: id,
+          name: name,
+          type: metaEvent.content.type,
+          children: {}
+        }
+      }
+
+      const typesOfSpaces = ['context',
+        'class',
+        'course',
+        'institution',
+        'degree program',
+        'design department',
+        'faculty',
+        'institute',
+        'semester']
+
+      async function scanForAndAddSpaceChildren (spaceId, path) {
+        if (spaceId === 'undefined') return
+        const stateEvents = await matrixClient.roomState(spaceId).catch(console.log)
+
+        // check if room exists in roomHierarchy
+        // const existsInCurrentTree = _.find(hierarchy, {room_id: spaceId})
+        // const metaEvent = await matrixClient.getStateEvent(spaceId, 'dev.medienhaus.meta')
+        const metaEvent = _.find(stateEvents, { type: 'dev.medienhaus.meta' })
+        if (!metaEvent) return
+        if (!typesOfSpaces.includes(metaEvent.content.type)) return
+
+        const nameEvent = _.find(stateEvents, { type: 'm.room.name' })
+        if (!nameEvent) return
+        const spaceName = nameEvent.content.name
+
+        // if (initial) {
+        _.set(result, [...path, spaceId], createSpaceObject(spaceId, spaceName, metaEvent, stateEvents))
+        // }
+
+        // const spaceSummary = await matrixClient.getSpaceSummary(spaceId)
+        console.log(`getting children for ${spaceId} / ${spaceName}`)
+        for (const event of stateEvents) {
+          if (event.type !== 'm.space.child' && !includeRooms) continue
+          if (event.type === 'm.space.child' && _.size(event.content) === 0) continue
+          if (event.room_id !== spaceId) continue
+          // if (event.sender !== process.env.RUNDGANG_BOT_USERID && !includeRooms) continue
+
+          // find deep where 'id' === event.room_id, and assign match to 'children'
+          // const path = findPathDeep(result, (room, key) => {
+          //   return room.id === event.room_id
+          // }, {
+          //   includeRoot: true,
+          //   rootIsChildren: true,
+          //   pathFormat: 'array',
+          //   childrenPath: 'children'
+          // })
+          //
+          // if (!path) continue
+
+          // const metaEvent = await matrixClient.getStateEvent(event.state_key, 'dev.medienhaus.meta')
+
+          // const childrenSpaceToAdd = createSpaceObject(event.state_key, spaceSummary, metaEvent)
+          // if (!childrenSpaceToAdd.name) continue
+
+          // _.set(result, [...path, 'children', event.state_key], childrenSpaceToAdd)
+
+          // result[...path, 'children'].push(childrenSpaceToAdd)
+          // const currentChildren = _.get(result, [...path, 'children'])
+          // if (!currentChildren) {
+          //   _.set(result, [...path, 'children'], [])
+          //   currentChildren = _.get(result, [...path, 'children'])
+          // }
+          // console.log(currentChildren)
+          // currentChildren.push(childrenSpaceToAdd)
+
+          // Check if this is a space itself, and if so try to get its children
+          // if (_.get(_.find(spaceSummary.rooms, ['room_id', event.state_key]), 'room_type') === 'm.space') {
+
+          await scanForAndAddSpaceChildren(event.state_key, [...path, spaceId, 'children'])
+          // }
+        }
+      }
+
+      await scanForAndAddSpaceChildren(motherSpaceRoomId, [])
+
+      return result
+    }
+    console.log('---- started structure ----')
+    const hierarchy = await props.matrixClient.getRoomHierarchy(parent, 50, 10)
+    const tree = await getSpaceStructure(props.matrixClient, parent, false, hierarchy)
+    console.log(tree)
+    setStructure(tree)
+  }
 
   const spaceChild = async (e, space, add) => {
     e && e.preventDefault()
@@ -70,11 +170,13 @@ const ManageContexts = (props) => {
     }
     createSpace(newContext)
   }
-
+  useEffect(() => {
+    createStructurObject()
+  }, [])
   return (
     <>
       <h2>Manage Contexts</h2>
-      <p>Crazy circles</p>
+      {structure ? <p>Crazy circles</p> : <Loading />}
       <label htmlFor="name">{t('Parent')}: </label>
       <input type="text" value={parentName} disabled />
       <RemoveContext t={t} selectedContext={selectedContext} parent={parent} parentName={parentName} disableButton={disableButton} callback={spaceChild} />
