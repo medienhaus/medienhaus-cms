26c11ce91c6cb8caa85be0334f523ee8f08ae6cc
Marcel <marcel-github@klasse.io>
2021-10-12 16:46:42 +0200

    add function to remove space from context

--- src/routes/admin/components/CreateContext.js
+++ src/routes/admin/components/CreateContext.js
@@ -4,13 +4,12 @@ import { Loading } from '../../../components/loading'
 function CreateContext (props) {
   return (
     <form>
-
       <div>
         <label htmlFor="name">{props.t('Context')}: </label>
         <input type="text" onChange={(e) => props.setNewContext(e.target.value)} />
       </div>
 
-      <button type="submit" onClick={(e) => props.callback(e)}>{props.loading ? <Loading /> : props.t('SUBMIT')}</button>
+      <button type="submit" disabled={props.disableButton} onClick={(e) => props.callback(e)}>{props.loading ? <Loading /> : props.t('Add Context')}</button>
     </form>
   )
 }
--- src/routes/admin/components/ManageContexts.js
+++ src/routes/admin/components/ManageContexts.js
@@ -1,18 +1,36 @@
 import React, { useState } from 'react'
 import { useTranslation } from 'react-i18next'
 import CreateContext from './CreateContext'
+import { RemoveContext } from './RemoveContext'
 
 const ManageContexts = (props) => {
   const { t } = useTranslation('admin')
+  const [selectedContext] = useState('!EyRTeozehwfsYePWMl:dev.medienhaus.udk-berlin.de')
   const [newContext, setNewContext] = useState('')
+  const [parentName] = useState('Stechlin')
   // eslint-disable-next-line no-unused-vars
-  const [loading, setLoading] = useState(false)
+  const [disableButton, setDisableButton] = useState(false)
   const [parent] = useState('!ZbMmIxgnJIhuROlgKJ:dev.medienhaus.udk-berlin.de')
 
-  function onSubmit (e) {
+  const spaceChild = async (e, space, add) => {
+    e && e.preventDefault()
+    const body = {
+      via:
+        [process.env.REACT_APP_MATRIX_BASE_URL],
+      suggested: false,
+      auto_join: true
+    }
+    await fetch(process.env.REACT_APP_MATRIX_BASE_URL + `/_matrix/client/r0/rooms/${parent}/state/m.space.child/${space}`, {
+      method: 'PUT',
+      headers: { Authorization: 'Bearer ' + localStorage.getItem('medienhaus_access_token') },
+      body: JSON.stringify(add ? body : { }) // if we add a space to an existing one we need to send the object 'body', to remove a space we send an empty object.
+    }).catch(console.log)
+  }
+
+  function addSpace (e) {
     e.preventDefault()
     const createSpace = async (title) => {
-      setLoading(true)
+      setDisableButton(true)
       const opts = (type, name, history) => {
         return {
           preset: 'private_chat',
@@ -37,26 +55,17 @@ const ManageContexts = (props) => {
             state_key: '',
             content: { guest_access: 'can_join' }
           }],
-          visibility: 'private'
+          visibility: 'public'
         }
       }
 
       // create the space for the context
       const space = await props.matrixClient.createRoom(opts('context', title, 'world_readable')).catch(console.log)
       // add this subspaces as children to the root space
-      await fetch(process.env.REACT_APP_MATRIX_BASE_URL + `/_matrix/client/r0/rooms/${parent}/state/m.space.child/${space.room_id}`, {
-        method: 'PUT',
-        headers: { Authorization: 'Bearer ' + localStorage.getItem('medienhaus_access_token') },
-        body: JSON.stringify({
-          via:
-                  [process.env.REACT_APP_MATRIX_BASE_URL],
-          suggested: false,
-          auto_join: true
-        })
-      }).catch(console.log)
+      spaceChild(e, space.room_id, true)
       console.log('created Context ' + newContext)
 
-      setLoading(false)
+      setDisableButton(false)
       return space
     }
     createSpace(newContext)
@@ -66,7 +75,10 @@ const ManageContexts = (props) => {
     <>
       <h2>Manage Contexts</h2>
       <p>Crazy circles</p>
-      <CreateContext t={t} parent={parent} matrixClient={props.matrixClient} setNewContext={setNewContext} loading={loading} callback={onSubmit} />
+      <label htmlFor="name">{t('Parent')}: </label>
+      <input type="text" value={parentName} disabled />
+      <RemoveContext t={t} selectedContext={selectedContext} parent={parent} parentName={parentName} disableButton={disableButton} callback={spaceChild} />
+      <CreateContext t={t} parent={parent} matrixClient={props.matrixClient} setNewContext={setNewContext} parentName={parentName} disableButton={disableButton} callback={addSpace} />
     </>
   )
 }
