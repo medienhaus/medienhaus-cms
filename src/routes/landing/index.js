ef427781f23212fe6f27eca4cf3ceab04613847b
Andi Rueckel <andirueckel@users.noreply.github.com>
2021-10-21 13:46:34 +0200

    add human-readable-first note to landing page

--- src/routes/landing/index.js
+++ src/routes/landing/index.js
@@ -12,21 +12,25 @@ const Landing = () => {
       <p>{t('Hello')}{profile && <strong> {profile?.displayname}</strong>}.</p>
       <h2>{t('Welcome to the Rundgang 2021 content management system!')}</h2>
       <p>{t('Please enter information about your Rundgang projects here. The platform will be activated for the public on October 25, 2021, so that visitors can plan their visit before the tour begins. Even after October 25, it is still possible to make adjustments to your project pages.')}</p>
+      <hr />
       <p>{t('The Rundgang – Open Days of the Berlin University of the Arts will take place for the first time in both analogue and digital space from 29–31 October 2021. On this platform, you will have the opportunity to create your own projects and thus add information on analogue offers or present digitally created semester projects.')}</p>
-      {auth.user && (
-        <>
-          <p>
-            <Trans t={t} i18nKey="bbbAndPeertube">
-              We also offer you the possibility to offer virtual meetings via the video conferencing tool BigBlueButton at <a href="https://meetings.udk-berlin.de" rel="external nofollow noopener noreferrer" target="_blank">udk/meet</a> and to integrate videos and live streams via <a href="https://stream.udk-berlin.de" rel="external nofollow noopener noreferrer" target="_blank">udk/stream</a> on your own project page.
-            </Trans>
-          </p>
-          <p>
-            <Trans t={t} i18nKey="information">
-              For more information on the Rundgang 2021 (opening hours, safety and hygiene measures, etc.), please visit: <a href="https://udk-berlin.de/rundgang" rel="external nofollow noopener noreferrer" target="_blank">udk-berlin.de/rundgang</a>
-            </Trans>
-          </p>
-        </>
-      )}
+      <hr />
+      <p>
+        <Trans t={t} i18nKey="information">
+          For more information on the Rundgang 2021 (opening hours, safety and hygiene measures, etc.), please visit: <a href="https://udk-berlin.de/rundgang" rel="external nofollow noopener noreferrer" target="_blank">udk-berlin.de/rundgang</a>
+        </Trans>
+      </p>
+      <hr />
+      <p>
+        <Trans t={t} i18nKey="human-readable-first">
+          All content is stored in compliance with the “human readable first” paradigm and can&thinsp;&mdash;&thinsp;besides the udk/rundgang content management system&thinsp;&mdash;&thinsp;be accessed, read, modified, and deleted with any fully-featured Matrix client via: <code>content.udk-berlin.de</code>
+        </Trans>
+      </p>
+      <p>
+        ❗️ <Trans t={t} i18nKey="caution">
+          Please note that manual content editing should be considered <em>advanced technique</em> and might break the content structure if not complying the <code>dev.medienhaus.meta</code> standard.
+        </Trans>
+      </p>
     </section>
   )
 }
