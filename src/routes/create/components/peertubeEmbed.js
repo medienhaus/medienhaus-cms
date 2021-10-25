30d44877373b4cad2a1f4f8c7ca90e6908429043
Marcel <marcel-klasse@users.noreply.github.com>
2021-10-25 17:43:45 +0200

    change stream. links to input field

--- src/routes/create/components/peertubeEmbed.js
+++ src/routes/create/components/peertubeEmbed.js
@@ -9,6 +9,7 @@ const PeertubeEmbed = ({ type, onCreateRoomForBlock, onBlockWasAddedSuccessfully
   const [loading, setLoading] = useState(false)
   const [entries, setEntries] = useState([])
   const [selectedEntry, setSelectedEntry] = useState('')
+  const [pastedEntry, setPastedEntry] = useState('')
   const matrixClient = Matrix.getMatrixClient()
   const username = matrixClient.getUserIdLocalpart()
   const { t } = useTranslation('peertube')
@@ -20,7 +21,7 @@ const PeertubeEmbed = ({ type, onCreateRoomForBlock, onBlockWasAddedSuccessfully
       const request = process.env.NODE_ENV === 'development' ? await fetch(`https://stream.udk-berlin.de/api/v1/accounts/d.erdmann/${resourceType}?count=100`) : await fetch(`https://stream.udk-berlin.de/api/v1/accounts/${username}/${resourceType}?count=100`)
       // TODO: pagination for more than 100 entries
       let entries = await request.json()
-      console.log(entries)
+      // console.log(entries)
       if (!(entries && entries.data && entries.data.length > 0)) {
         setEntries([])
         setSelectedEntry('')
@@ -55,10 +56,18 @@ const PeertubeEmbed = ({ type, onCreateRoomForBlock, onBlockWasAddedSuccessfully
     onBlockWasAddedSuccessfully()
   }
 
+  /*
   function selectEntry (e) {
     setSelectedEntry(e.target.value)
     !saveButton && callback(e.target.value)
   }
+*/
+  function pasteEntry (e) {
+    console.log(e.target.value.substring(e.target.value.lastIndexOf('/') + 1))
+    setSelectedEntry(e.target.value.substring(e.target.value.lastIndexOf('/') + 1))
+    setPastedEntry(e.target.value)
+  }
+  const validInputValue = pastedEntry.startsWith('https://stream.udk-berlin.de/')
 
   if (loading) {
     return <Loading />
@@ -66,6 +75,7 @@ const PeertubeEmbed = ({ type, onCreateRoomForBlock, onBlockWasAddedSuccessfully
 
   return (
     <div>
+      {/*
       <select disabled={entries.length === 0} onChange={selectEntry} value={selectedEntry}>
         <option value="" disabled>
           {(
@@ -77,11 +87,18 @@ const PeertubeEmbed = ({ type, onCreateRoomForBlock, onBlockWasAddedSuccessfully
         {entries.map(entry => (
           <option value={entry.uuid} key={entry.uuid}>{(type === 'playlist' ? entry.displayName : entry.name)}</option>
         ))}
-      </select>
+        <option value={pastedEntry.substring(pastedEntry.lastIndexOf('/') + 1)} key={pastedEntry}>{pastedEntry}</option>
+
+        </select>
+        */
+      }
+      <p>{t('Paste a link to your')} {type}</p>
+      <input type="text" value={pastedEntry} onChange={pasteEntry} placeholder="https://stream.udk-berlin.de/videos/watch/..." />
       {type === 'video' && <p>↳ <Trans t={t} i18nKey="linkToVideo">You can upload videos via <a href="https://stream.udk-berlin.de/videos/upload" rel="external nofollow noopener noreferrer" target="_blank">udk/stream</a></Trans></p>}
       {type === 'livestream' && <p>↳ <Trans t={t} i18nKey="linkToStream">You can start a live stream via <a href="https://stream.udk-berlin.de/videos/upload" rel="external nofollow noopener noreferrer" target="_blank">udk/stream</a></Trans></p>}
       {type === 'playlist' && <p>↳ <Trans t={t} i18nKey="linkToPlaylist">You can create playlists via <a href="https://stream.udk-berlin.de/videos/upload" rel="external nofollow noopener noreferrer" target="_blank">udk/stream</a></Trans></p>}
-      {saveButton && <LoadingSpinnerButton onClick={handleSubmit} disabled={entries.length < 1 || !selectedEntry}>Add Content</LoadingSpinnerButton>}
+
+      {saveButton && <LoadingSpinnerButton onClick={handleSubmit} disabled={entries.length < 1 || !selectedEntry || !validInputValue}>Add Content</LoadingSpinnerButton>}
     </div>
   )
 }
