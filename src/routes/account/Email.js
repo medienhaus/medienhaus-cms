871007e8987c211d2694fa839b9a044b799bf545
Marcel <marcel-klasse@users.noreply.github.com>
2022-02-13 16:50:17 +0100

    recover email

--- src/routes/account/Email.js
+++ src/routes/account/Email.js
@@ -11,7 +11,7 @@ export const Email = () => {
   const sendAttempt = useRef(0)
   const matrixClient = Matrix.getMatrixClient()
 
-  function isEmail(val) {
+  function isEmail (val) {
     const regEmail = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
     if (!regEmail.test(val)) {
       return true
