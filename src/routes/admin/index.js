0d2cb39ef2b648aef559147cd716785f71fa1688
Florian Weber <florian@weber.world>
2022-02-23 16:21:03 +0100

    Partially revert 41beff9fa7511fb7feba6f556909765155c57155 to bring back /admin page functionality

--- src/routes/admin/index.js
+++ src/routes/admin/index.js
@@ -2,16 +2,15 @@ import React, { useEffect, useState } from 'react'
 import { Loading } from '../../components/loading'
 import Matrix from '../../Matrix'
 
+import AddUser from './components/AddUser'
+import ChangePassword from './components/ChangePassword'
+import DeleteUser from './components/DeleteUser'
 import { useTranslation, Trans } from 'react-i18next'
-import ManageContexts from './components/ManageContexts'
-
-// import { useAuth } from '../../Auth'
-
-// const auth = useAuth()
 
 const Admin = () => {
   const [admin, setAdmin] = useState(false)
   const [loading, setLoading] = useState(false)
+  const [selection, setSelection] = useState('add')
   const { t } = useTranslation('admin')
 
   const matrixClient = Matrix.getMatrixClient()
@@ -26,13 +25,37 @@ const Admin = () => {
     checkAdminPriviliges()
   }, [admin, matrixClient])
 
+  const renderSelection = () => {
+    switch (selection) {
+      case 'password':
+        return <ChangePassword />
+      case 'delete':
+        return <DeleteUser />
+      default:
+        return <AddUser matrixClient={matrixClient} />
+    }
+  }
+
   if (!matrixClient || loading) return <Loading />
   if (!admin) return <Trans t={t} i18nKey="adminPriviliges">You need admin priviliges to see this page.</Trans>
 
   return (
     <>
       <section className="admin">
-        <ManageContexts matrixClient={matrixClient} />
+        <h2>{t('What do you want to do?')}</h2>
+        <select className="radio" onChange={(event) => setSelection(event.target.value)}>
+          <option id="add-user" value="add">
+            {t('Add Account')}
+          </option>
+          <option id="change-password" value="password">
+            {t('Reset Password')}
+          </option>
+          <option id="delete-user" value="delete">
+            {t('Delete User')}
+          </option>
+        </select>
+        <hr />
+        {renderSelection()}
       </section>
     </>
   )
