4209ee909bf196601a0e0f682e22486c75dc3366
Florian Weber <florian@weber.world>
2022-05-17 18:26:33 +0200

    Tidy up peertubeEmbed.js even more...

--- src/routes/create/components/peertubeEmbed.js
+++ src/routes/create/components/peertubeEmbed.js
@@ -3,37 +3,34 @@ import Matrix from '../../../Matrix'
 import LoadingSpinnerButton from '../../../components/LoadingSpinnerButton'
 import { Trans, useTranslation } from 'react-i18next'
 
-const PeertubeEmbed = ({ type, onCreateRoomForBlock, onBlockWasAddedSuccessfully, saveButton, callback }) => {
-  const [selectedEntry, setSelectedEntry] = useState('')
-  const [pastedEntry, setPastedEntry] = useState('')
+const PeertubeEmbed = ({ type, onCreateRoomForBlock, onBlockWasAddedSuccessfully }) => {
+  const [inputValue, setInputValue] = useState('')
   const matrixClient = Matrix.getMatrixClient()
   const { t } = useTranslation('peertube')
 
   async function handleSubmit () {
     const blockRoomId = await onCreateRoomForBlock()
     await matrixClient.sendMessage(blockRoomId, {
-      body: selectedEntry,
+      body: inputValue.substring(inputValue.lastIndexOf('/') + 1),
       msgtype: 'm.text'
     })
     onBlockWasAddedSuccessfully()
   }
 
   function pasteEntry (e) {
-    setSelectedEntry(e.target.value.substring(e.target.value.lastIndexOf('/') + 1))
-    setPastedEntry(e.target.value)
-    !saveButton && callback(e.target.value)
+    setInputValue(e.target.value)
   }
-  const validInputValue = pastedEntry.startsWith('https://stream.udk-berlin.de/')
+  const validInputValue = inputValue.startsWith('https://stream.udk-berlin.de/')
 
   return (
     <div>
       <p>{t('Paste a link to your {{type}} below', { type })}:</p>
-      <input type="text" value={pastedEntry} onChange={pasteEntry} placeholder="https://stream.udk-berlin.de/…" />
+      <input type="text" value={inputValue} onChange={pasteEntry} placeholder="https://stream.udk-berlin.de/…" />
       {type === 'video' && <p>↳ <Trans t={t} i18nKey="linkToVideo">You can upload videos via <a href="https://stream.udk-berlin.de/videos/upload" rel="external nofollow noopener noreferrer" target="_blank">udk/stream</a></Trans></p>}
       {type === 'livestream' && <p>↳ <Trans t={t} i18nKey="linkToStream">You can start a live stream via <a href="https://stream.udk-berlin.de/videos/upload" rel="external nofollow noopener noreferrer" target="_blank">udk/stream</a></Trans></p>}
       {type === 'playlist' && <p>↳ <Trans t={t} i18nKey="linkToPlaylist">You can create playlists via <a href="https://stream.udk-berlin.de/videos/upload" rel="external nofollow noopener noreferrer" target="_blank">udk/stream</a></Trans></p>}
 
-      {saveButton && <LoadingSpinnerButton onClick={handleSubmit} disabled={!selectedEntry || !validInputValue}>Add Content</LoadingSpinnerButton>}
+      <LoadingSpinnerButton onClick={handleSubmit} disabled={!validInputValue}>Add Content</LoadingSpinnerButton>
     </div>
   )
 }
