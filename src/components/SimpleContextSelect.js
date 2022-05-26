50b70a2f09a52a745bfa4fab9408a6461face664
Andi Rueckel <andirueckel@users.noreply.github.com>
2022-05-26 09:51:31 +0200

    use <input> & <datalist> instead of <select> in components/SimpleContextSelect.js

--- src/components/SimpleContextSelect.js
+++ src/components/SimpleContextSelect.js
@@ -10,33 +10,36 @@ function SimpleContextSelect ({ onItemChosen, selectedContext, contexts, struktu
   const items = config.medienhaus?.sites?.moderate?.manageContexts?.showRoot ? struktur : struktur[Object.keys(struktur)[0]].children
 
   return (
-    <select disabled={disabled} defaultValue={selectedContext} onChange={(e) => { onItemChosen(e.target.value) }}>
-      <option disabled value="">-- {t('select context')} --</option>
-      {mapDeep(filterDeep(items, (value, key, parent, context) => {
-        // Exclude all hierarchy elements that are not "contexts"
-        if (!value?.type.includes('context')) return false
-        value.path = []
-        value.pathIds = []
-        function addParentToPath (item) {
-          if (item.parentItem.value.name) {
-            value.path.unshift(item.parentItem.value.name)
-            value.pathIds.unshift(item.parentItem.value.id)
-            // Recursion: If this parent has yet another parent item, go check that out
-            if (item.parentItem.parentItem) {
-              addParentToPath(item.parentItem)
+    <>
+      <input id="context-select" list="context-list" name="context-select" placeholder="-- type to search / double-click to select --" type="text" />
+      <datalist id="context-list" disabled={disabled} defaultValue={selectedContext} onChange={(e) => { onItemChosen(e.target.value) }}>
+        <option disabled value="">-- {t('select context')} --</option>
+        {mapDeep(filterDeep(items, (value, key, parent, context) => {
+          // Exclude all hierarchy elements that are not "contexts"
+          if (!value?.type.includes('context')) return false
+          value.path = []
+          value.pathIds = []
+          function addParentToPath (item) {
+            if (item.parentItem.value.name) {
+              value.path.unshift(item.parentItem.value.name)
+              value.pathIds.unshift(item.parentItem.value.id)
+              // Recursion: If this parent has yet another parent item, go check that out
+              if (item.parentItem.parentItem) {
+                addParentToPath(item.parentItem)
+              }
             }
           }
-        }
-        addParentToPath(context._item)
-        if (!config.medienhaus?.sites?.moderate?.manageContexts?.showRoot) {
-          value.path.unshift(struktur[Object.keys(struktur)[0]].name)
-          value.pathIds.unshift(struktur[Object.keys(struktur)[0]].id)
-        }
-        return true
-      }, { childrenPath: 'children', includeRoot: false, rootIsChildren: true }), (value, key, parent, context) => (
-        <option key={value.id + key} disabled={(contexts && contexts.some(context => context.room_id === value.id)) || (moderationRooms && !moderationRooms.some(room => room.room_id === value.id))} value={value.id}>{' --- '.repeat(context.depth - 1)}{value.name}</option>
-      ), { childrenPath: 'children', includeRoot: false, rootIsChildren: true })}
-    </select>
+          addParentToPath(context._item)
+          if (!config.medienhaus?.sites?.moderate?.manageContexts?.showRoot) {
+            value.path.unshift(struktur[Object.keys(struktur)[0]].name)
+            value.pathIds.unshift(struktur[Object.keys(struktur)[0]].id)
+          }
+          return true
+        }, { childrenPath: 'children', includeRoot: false, rootIsChildren: true }), (value, key, parent, context) => (
+          <option key={value.id + key} disabled={(contexts && contexts.some(context => context.room_id === value.id)) || (moderationRooms && !moderationRooms.some(room => room.room_id === value.id))} value={value.id}>{' --- '.repeat(context.depth - 1)}{value.name}</option>
+        ), { childrenPath: 'children', includeRoot: false, rootIsChildren: true })}
+      </datalist>
+    </>
   )
 }
 
