36c20d3ad1056f70ba755e02370dfca79f04281a
Marcel <marcel-klasse@users.noreply.github.com>
2022-06-15 18:55:36 +0200

    add danger zone styling

--- src/routes/admin/components/RemoveContext.js
+++ src/routes/admin/components/RemoveContext.js
@@ -4,24 +4,26 @@ import styled from 'styled-components'
 import { useTranslation } from 'react-i18next'
 
 const RemoveCheckbox = styled.div`
-display: flex;
-justify-content: space-between;
-align-items: center;
+  display: grid;
+  grid-auto-flow: column;
+  grid-gap: var(--margin);
+  align-items: center;
+  justify-content: space-between;
 `
+
 export function RemoveContext (props) {
   const [checkbox, setCheckbox] = useState(false)
   const { t } = useTranslation('moderate')
   return (
-    <div>
-      <h3>DANGER ZONE</h3>
-      <form>
-        <RemoveCheckbox>
-          <label htmlFor="hide-authors">{t('I am sure I want to irreversibly delete this context.')}</label>
-          <input id="checkbox" name="checkbox" type="checkbox" checked={checkbox} onChange={() => setCheckbox(checkbox => !checkbox)} />
-        </RemoveCheckbox>
-        <button type="submit" disabled={props.disableButton || !checkbox} onClick={e => props.callback(e, props.parent, false)}>{props.loading ? <Loading /> : props.t('Remove Context')}</button>
-      </form>
-    </div>
+    <>
+      <h3>⚠️&nbsp;&nbsp;DANGER ZONE&nbsp;‼️</h3>
+      <RemoveCheckbox>
+        <label htmlFor="checkbox">{t('I am sure I want to irreversibly delete this context.')}</label>
+        <input id="checkbox" name="checkbox" type="checkbox" checked={checkbox} onChange={() => setCheckbox(checkbox => !checkbox)} />
+      </RemoveCheckbox>
+      <button type="submit" disabled={props.disableButton || !checkbox} onClick={e => props.callback(e, props.parent, false)}>{props.loading ? <Loading /> : props.t('Delete context')}</button>
+    </>
   )
 }
+
 export default RemoveContext
