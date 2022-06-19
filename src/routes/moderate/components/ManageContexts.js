9f9dd1676327e93fd758ff17e915b172f5f4b193
Marcel <marcel-klasse@users.noreply.github.com>
2022-06-19 11:06:36 +0200

    add translations, some error handleing and finish change template function, add list to medienhaus helper

--- src/routes/moderate/components/ManageContexts.js
+++ src/routes/moderate/components/ManageContexts.js
@@ -63,7 +63,8 @@ const ManageContexts = ({ matrixClient, moderationRooms: incomingModerationRooms
   const [moderationRooms, setModerationRooms] = useState(incomingModerationRooms)
 
   const [editRoomName, setEditRoomName] = useState(false)
-  const [newRoomName, setNewRoomName] = useState()
+  const [newRoomName, setNewRoomName] = useState('')
+  const [error, setError] = useState('')
 
   const createStructurObject = async () => {
     async function getSpaceStructure (matrixClient, motherSpaceRoomId, includeRooms) {
@@ -125,7 +126,10 @@ const ManageContexts = ({ matrixClient, moderationRooms: incomingModerationRooms
   }
 
   const fetchTreeFromApi = async () => {
-    const tree = await fetchTree(process.env.REACT_APP_CONTEXT_ROOT_SPACE_ID)
+    const tree = await fetchTree(process.env.REACT_APP_CONTEXT_ROOT_SPACE_ID).catch(() => {
+      setError('❗️' + t('An error occured, please try again or try reloading the page.'))
+      setModerationRooms({})
+    })
     setInputItems(tree)
     const _moderationRooms = { ...moderationRooms }
     for (const space of Object.keys(incomingModerationRooms)) {
@@ -381,8 +385,13 @@ const ManageContexts = ({ matrixClient, moderationRooms: incomingModerationRooms
     setEditRoomName(false)
   }
 
-  const onChangeRoomTemplate = async () => {
-    await matrixClient.getStateEvent(selectedContext, 'dev.medienhaus.meta')
+  const onChangeRoomTemplate = async (e) => {
+    const oldTemplate = roomTemplate
+    setRoomTemplate(e.target.value)
+    const metaEvent = await matrixClient.getStateEvent(selectedContext, 'dev.medienhaus.meta')
+    metaEvent.template = e.target.value
+    const send = await matrixClient.sendStateEvent(selectedContext, 'dev.medienhaus.meta', metaEvent)
+    if (!send.event_id) setRoomTemplate(oldTemplate)
     if (config.medienhaus.api) triggerApiUpdate(selectedContext)
   }
 
@@ -436,6 +445,7 @@ const ManageContexts = ({ matrixClient, moderationRooms: incomingModerationRooms
               disabled={loading}
             />}
         {loading && inputItems && <Loading />}
+        {error && <section>{error}</section>}
         {/* <label htmlFor="name">{t('Context')}: </label>
          <input type="text" value={selectedContextName} disabled /> */}
         {selectedContext &&
