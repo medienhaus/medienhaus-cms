127ef759438b80c2ddb1d9a823bd50eb3d7717a5
Florian Weber <florian@weber.world>
2022-08-25 14:14:06 +0200

    Fix indentation

--- src/Matrix.js
+++ src/Matrix.js
@@ -76,52 +76,52 @@ class Matrix {
     return rooms
   }
 
-createRoom = async (name, isSpace, topic, joinRule, type, template) => {
-  const opts = {
-    name: name,
-    room_version: '9',
-    preset: 'private_chat',
-    topic: topic,
-    visibility: 'private', // by default we want rooms and spaces to be private, this can later be changed either in /content or /moderate
-    creation_content: {
-      type: isSpace ? 'm.space' : 'm.room'
-    },
-    initial_state: [{
-      type: 'm.room.history_visibility',
-      content: { history_visibility: 'world_readable' } // history has to be world_readable so content of the rooms is visible for everyone who joins the room at a later point in time
-    },
-    {
-      type: 'm.room.join_rules',
-      content: { join_rule: joinRule } // can be set to either public, invite or knock
-    }],
-    power_level_content_override: {
+  createRoom = async (name, isSpace, topic, joinRule, type, template) => {
+    const opts = {
+      name: name,
+      room_version: '9',
+      preset: 'private_chat',
+      topic: topic,
+      visibility: 'private', // by default we want rooms and spaces to be private, this can later be changed either in /content or /moderate
+      creation_content: {
+        type: isSpace ? 'm.space' : 'm.room'
+      },
+      initial_state: [{
+        type: 'm.room.history_visibility',
+        content: { history_visibility: 'world_readable' } // history has to be world_readable so content of the rooms is visible for everyone who joins the room at a later point in time
+      },
+      {
+        type: 'm.room.join_rules',
+        content: { join_rule: joinRule } // can be set to either public, invite or knock
+      }],
+      power_level_content_override: {
       // we only want users with moderation rights to be able to do any actions, people joining the room will have a default level of 0.
-      ban: 50,
-      events: {
-        'm.room.name': 50,
-        'm.room.power_levels': 50
-      },
-      events_default: 50,
-      invite: 50,
-      kick: 50,
-      notifications: {
-        room: 20
-      },
-      redact: 50,
-      state_default: 50,
-      users_default: 0
+        ban: 50,
+        events: {
+          'm.room.name': 50,
+          'm.room.power_levels': 50
+        },
+        events_default: 50,
+        invite: 50,
+        kick: 50,
+        notifications: {
+          room: 20
+        },
+        redact: 50,
+        state_default: 50,
+        users_default: 0
+      }
     }
-  }
 
-  const room = await this.matrixClient.createRoom(opts)
-  const medienhausMetaEvent = {
-    type: type,
-    template: template,
-    version: '0.4'
+    const room = await this.matrixClient.createRoom(opts)
+    const medienhausMetaEvent = {
+      type: type,
+      template: template,
+      version: '0.4'
+    }
+    await this.matrixClient.sendStateEvent(room.room_id, 'dev.medienhaus.meta', medienhausMetaEvent)
+    return room
   }
-  await this.matrixClient.sendStateEvent(room.room_id, 'dev.medienhaus.meta', medienhausMetaEvent)
-  return room
-}
 }
 
 export default new Matrix()
